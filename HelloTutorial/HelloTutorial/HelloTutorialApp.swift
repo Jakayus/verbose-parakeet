04c1210faa512d422aa8cccc5c58e5945a8402ba
Joel Sereno <jakayus123@gmail.com>
2023-11-11 18:21:54 -0700

    initial commit

---
+++ HelloTutorial/HelloTutorial/HelloTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTutorialApp.swift
+//  HelloTutorial
+//
+//  Created by Joel Sereno on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
