78ad3f33cc8d9c1b2e4fa9c4cb67cc41ac78353b
Joel Sereno <jakayus123@gmail.com>
2023-11-12 18:09:25 -0700

    add demo project

---
+++ AppDocTest/AppDocTest/AppDocTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDocTestApp.swift
+//  AppDocTest
+//
+//  Created by Joel Sereno on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AppDocTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
