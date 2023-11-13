7e1569ecd79419215b53dd8fc7e4eb9a11c94170
Joel Sereno <jakayus123@gmail.com>
2023-11-12 23:25:22 -0700

    duplication cleanup

--- build-docc.sh
+++ build-docc.sh
@@ -1,7 +1,5 @@
 ##!/bin/sh
 
-cd AppDocTest
-
 xcrun xcodebuild docbuild \
     -scheme AppDocTest \
     -destination 'generic/platform=iOS Simulator' \
@@ -10,7 +8,7 @@ xcrun xcodebuild docbuild \
 xcrun docc process-archive transform-for-static-hosting \
     "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/AppDocTest.doccarchive" \
     --output-path ".docs" \
-    --hosting-base-path "verbose-parakeet" # add your repo name later
+    --hosting-base-path "" # add your repo name later
 
 echo '<script>window.location.href += "/documentation/appdoctest"</script>' > .docs/index.html
 
