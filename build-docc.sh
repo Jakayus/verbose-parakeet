cb05115445457668f7d5189dd55f680cf13315f0
Joel Sereno <jakayus123@gmail.com>
2023-11-20 22:49:19 -0700

    updating temp yaml

--- build-docc.sh
+++ build-docc.sh
@@ -8,9 +8,9 @@ xcrun xcodebuild docbuild \
 xcrun docc process-archive transform-for-static-hosting \
     "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/AppDocTest.doccarchive" \
     --output-path ".docs" \
-    --hosting-base-path "verbose-parakeet" # add your repo name later
+    --hosting-base-path "" # add your repo name later
 
-echo '<script>window.location.href += "/documentation/appdoctest"</script>' > .docs/index.html
+#echo '<script>window.location.href += "/documentation/appdoctest"</script>' > .docs/index.html
 
 
 
